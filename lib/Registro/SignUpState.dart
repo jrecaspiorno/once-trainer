92edd0a7d4231bfdd763dc14d87487ef6856ef3c
RicardoRodrigoRuiz <57674701+RicardoRodrigoRuiz@users.noreply.github.com>
2020-05-25 14:17:06 +0200

    inicio de sesion se guarda

--- lib/Registro/SignUpState.dart
+++ lib/Registro/SignUpState.dart
@@ -5,19 +5,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:googleapis/people/v1.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 class LoginState with ChangeNotifier{
-   GoogleSignIn _googleSingIn = GoogleSignIn( );
+  GoogleSignIn _googleSingIn = GoogleSignIn( );
+  SharedPreferences _prefs;
   final _auth = FirebaseAuth.instance;
   bool _fecha_introducida = false;
   bool _logedIn = false;
-  bool _loading = false;
+  bool _loading = true;
   FirebaseUser _user;
   bool isLoading() => _loading;
   DateTime _date;
   bool isLogedIn() => _logedIn;
   FirebaseUser currentUser() => _user;
   bool getFecha() => _fecha_introducida;
-
+  LoginState() {
+    loginState();
+  }
   Future<Date> _getBDay() async{
 
     var client = http.Client();
@@ -44,6 +48,7 @@ class LoginState with ChangeNotifier{
     print("Birthday from " + _user.displayName + " " + _date.toString()) ;
     _loading = false;
     if(_user != null) {
+      _prefs.setBool('isLoggedIn', true);
       _logedIn = true;
       notifyListeners();
     }else{
@@ -52,6 +57,7 @@ class LoginState with ChangeNotifier{
     }
   }
   void logout(){
+    _prefs.clear();
     _googleSingIn.signOut();
     _logedIn = false;
     notifyListeners();
@@ -67,6 +73,20 @@ class LoginState with ChangeNotifier{
     final FirebaseUser user =(await _auth.signInWithCredential(credential)).user;
     print("signed in " + user.displayName);
     return user;
+  }
+
+  void loginState() async {
+    _prefs = await SharedPreferences.getInstance();
+    if(_prefs.containsKey('isLoggedIn')){
+      _user = await _auth.currentUser();
+
+      _logedIn = _user != null;
+      _loading = false;
+      notifyListeners();
+    }else{
+      _loading = false;
+      notifyListeners();
+    }
   }
 
 
