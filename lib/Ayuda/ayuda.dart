e29bb47af1a5b49980b1ab2d275d9dc591685394
sistematiko5 <35228801+sistematiko5@users.noreply.github.com>
2020-07-29 12:45:51 +0200

    Manual terminado

--- lib/Ayuda/ayuda.dart
+++ lib/Ayuda/ayuda.dart
@@ -14,17 +14,26 @@ class MyHelp extends StatelessWidget {
         title: Text("Historial Clinico"),
         backgroundColor: Colors.indigo,
       ),
-      body: Center(
-        child: Text('1. Instrucciones generales: Es necesario conexión a internet solo la primera vez hasta que se inicie sesión con una cuenta valida, '
-              'tener instalado en el dispositivo Google Fit y aceptar los permisos de Google.\n'
-            '2. Menú principal: Se compone de cuatro botones, recomendador, lista de ejercicios, perfil y ayuda\n'
-            '3. Perfil: Dispone de otros cuatro botones, el historial de actividades, historial clínico, los ajustes y desconectarse.\n'
-            '4. Historial Actividades: Se muestra la información de la actividad fisica registrad por laa aplicación.\n'
-            '5. Historial clinico: Hay disponible información sobre el peso, el ritmo cardiaco y las energias quemadas. La información se mostrara en forma de grafica y escrita para que sea accesible para el usuario.\n'
-            '6. Ajustes: En la ventana ajustes se compone de 5 botones para poner las dolencias del usuario, modificar la fecha de nacimiento, cambiar el nivel de dificultad, crear una copia de seguridad asi como recuperar los datos.\n'
-            '7. Lista de ejercicios: En esta ventana se encuentran todos los ejercicios de la aplicación. Si el usuario entra en un ejercicio podra realizarlo y cuando terminé pulsara el boton de hecho.\n'
-            '8. Recomendador: El recomendador recomienda ejercicios diferentes cada día para que el usuario realize sesiones de entrenamiento equilibradas.\n'
-            '9. Alerta ritmo cárdiaco: Al realizar cualquier ejercicio si el usuario supera su frecuencia maxima cardiaca saltará una alerta, tanto visual como sonora, para que el usuaro reduzca la actividad física.\n'),
+
+      body: Container(
+        margin: EdgeInsets.only(left: 10,top:5,right:5),
+
+        child: ListView(
+
+          children:  <Widget>[
+            Text('1. Instrucciones generales: Es necesario conexión a internet solo la primera vez hasta que se inicie sesión con una cuenta válida, '
+                  'tener instalado en el dispositivo Google Fit y aceptar los permisos de Google.\n', style: TextStyle(fontSize: 17)),
+            Text( '2. Menú principal: Se compone de cuatro botones, recomendador, lista de ejercicios, perfil y ayuda.\n', style: TextStyle(fontSize: 17)),
+            Text( '3. Perfil: Dispone de otros cuatro botones, el historial de actividades, historial clínico, los ajustes y desconectarse.\n', style: TextStyle(fontSize: 17)),
+            Text( '4. Historial Actividades: Se muestra la información de la actividad fisica registrad por la aplicación.\n', style: TextStyle(fontSize: 17)),
+            Text(  '5. Historial clínico: Hay disponible información sobre el peso, el ritmo cardiaco y las energias quemadas. La información se mostrará en forma de gráfica y escrita para que sea accesible para el usuario.\n', style: TextStyle(fontSize: 17)),
+            Text('6. Ajustes: En la ventana ajustes se compone de cinco botones para poner las dolencias del usuario, modificar la fecha de nacimiento, cambiar el nivel de dificultad, '
+                   'crear una copia de seguridad asi como recuperar los datos.\n', style: TextStyle(fontSize: 17)),
+            Text(  '7. Lista de ejercicios: En esta ventana se encuentran todos los ejercicios de la aplicación. Si el usuario entra en un ejercicio podra realizarlo y cuando terminé pulsará el botón de hecho.\n', style: TextStyle(fontSize: 17)),
+            Text( '8. Recomendador: Muestra ejercicios personalizados diferentes cada día para que el usuario realize sesiones de entrenamiento equilibradas.\n', style: TextStyle(fontSize: 17)),
+            Text( '9. Alerta ritmo cárdiaco: Al realizar cualquier ejercicio si el usuario supera su frecuencia máxima cardiaca saltará una alerta, tanto visual como sonora, para que el usuario reduzca la actividad física.\n', style: TextStyle(fontSize: 17)),
+          ]
+        ),
       )
     );
   }
